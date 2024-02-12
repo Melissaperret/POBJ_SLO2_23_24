9674a7f8e2af0bac4aa7348281afaa49d4d771de
Melissa Perret <Melissa.perret@eduvaud.ch>
2024-02-12 15:29:31 +0100

    Correction bug + fixe Code Ex 1 fonctionnel :)

--- POBJ/Csharp/Ex_1/Ex_1/Program.cs
+++ POBJ/Csharp/Ex_1/Ex_1/Program.cs
@@ -3,9 +3,9 @@
 // 28.11.2023 Mélissa Perret 
 
 using System;
+using System.Globalization;
 
-
-    public class Program
+public class Program
 {
     static void Main(string[] args)
     {
@@ -17,25 +17,21 @@ using System;
         double ValB2;
 
         Console.WriteLine("Ecercice:Perret Mélissa"); 
-        Console.WriteLine("Solution en C a trandformer en C#");
+        Console.WriteLine("Solution en C a transformer en C#");
 
         do
         {
             Console.WriteLine("Test A ou B, Q pour Quitter");
-            UserAnswer = Console.ReadKey().KeyChar;
-
-            //-> majuscule 
-            UserAnswer = char.ToUpper(UserAnswer); 
+            UserAnswer = Console.ReadLine().ToUpper()[0];
 
             switch (UserAnswer)
             {
                 case 'A':
-                    
-                    Console.WriteLine("\nTestA: entrez un nombre entre 1 et 9");
-                    ValA = Console.Read();
-                    ValA = int.Parse(ValA);
 
-                    if (ValA > 9)
+                    Console.WriteLine("Test A: entrez un nombre entre 1 et 9");
+                    if (int.TryParse(Console.ReadLine(), out ValA))
+
+                        if (ValA > 9)
                     {
                         ValA = 9;
                         Console.WriteLine("TestA: ValA limitée à 9");
@@ -61,9 +57,9 @@ using System;
                     break;
 
                 case 'B':
-                   
-                    Console.WriteLine("\nTestB: entrez une valeur entre 0 et 9");
-                    ValB = Console.Read();
+
+                    Console.WriteLine("Test B: entrez une valeur entre 0 et 9");
+                    if (int.TryParse(Console.ReadLine(), out ValB))
 
                     if (ValB >= 0 && ValB <= 9)
                     {
@@ -71,9 +67,10 @@ using System;
                         {
                             ValB1 = (short)(100 + (10 * i));
                             ValB2 = ValB1 / 10000.0;
-                            Console.WriteLine($"TestB: i= {i} ValB1= {ValB1:F5} ValB2= {ValB2}");
+                            Console.WriteLine($"Test B: i= {i} ValB1= {ValB1:D3} ValB2= {ValB2.ToString("F6", CultureInfo.InvariantCulture)}");
+
+                            }
                         }
-                    }
                     else
                     {
                         Console.WriteLine("TestB: ValB n'est pas entre 0 et 9 !");
