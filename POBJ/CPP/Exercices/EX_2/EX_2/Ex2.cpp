e1937b06524668fd91f98fcc0152b00250802a70
Nicolas Regnault <nicolas@oddtales.net>
2024-02-14 19:06:47 +0100

    Code terminé et fonctionnel

--- POBJ/CPP/Exercices/EX_2/EX_2/Ex2.cpp
+++ POBJ/CPP/Exercices/EX_2/EX_2/Ex2.cpp
@@ -23,26 +23,47 @@ unsigned short int Extract(unsigned long totalSecondes, unsigned short int heure
 }
 
 // Fonction AfficheB
-void AfficheB(int ValA, unsigned short int heures, unsigned short int minutes, unsigned short int resteSecondes) 
+void AfficheB(long valeur, char format = 'D', short int tailleChamp = 5)
 {
-	cout << "ValA = " << ValA << " NbHeures = " << heures << " NbMinutes = " << minutes << " NbSecondes = " << resteSecondes << endl;
+	if (format != 'H')  // Vérifie si le format n'est pas hexadécimal
+	{
+		// Affiche la valeur en décimal avec une largeur de champ spécifiée
+		cout << "Nombre = " << setw(tailleChamp) << dec << valeur << " en decimal" << endl;
+	}
+	else
+	{
+		// Si le format est hexadécimal, affiche la valeur en hexadécimal avec une largeur de champ spécifiée
+		cout << "Nombre = " << setw(tailleChamp) << hex << valeur << " en hexa" << endl;
+	}
 }
-
 // Fonction CalculSurfaceRectangle
+int CalculSurfaceRectangleInt(int hauteur, int largeur)
+{
+	return hauteur * largeur;
+}
+double CalculSurfaceRectangleDouble(double hauteur, double largeur)
+{
+	return hauteur * largeur;
+}
+int CalculSurfaceRectangleAvecQuatreArguments(int X1, int Y1, int X2, int Y2)
+{
+	return (abs(X2 - X1) * abs(Y2 - Y1));
+}
 
 
 int main (void)
 {
 	char UserAnswer;
-	int ValA, ValB;
+	int ValA; 
+	long ValB; 
+
 	// Variables pour test A, B et C
 	unsigned short int resteSecondes = 0; // Déclaration de resteSecondes en dehors du switch
-	unsigned short int heures, minutes;
 	unsigned short int heures = 0; // Initialisation de heures à 0
 	unsigned short int minutes = 0; // Initialisation de minutes à 0
 
 	// a adapter
-	cout << "Exercice 2 : Mélissa Perret" << endl;
+	cout << "Exercice 2 : Melissa Perret" << endl;
 
 
 	do {
@@ -56,27 +77,38 @@ int main (void)
 				cin >> ValA;
 
 				resteSecondes = Extract(ValA, heures, minutes);
-				AfficheB(ValA, heures, minutes, resteSecondes);
+				cout << "ValA = " << ValA << " NbHeures = " << heures << " NbMinutes = " << minutes << " NbSecondes = " << resteSecondes << endl;
 				
 			break;
 
-			//case 'B':
-			//case 'b':
-			//	cout << "TestB: entrez un nombre " <<  endl;
-			//	cin >> ValB; 
-			//	
-			//break;
+			case 'B':
+			case 'b':
+				cout << "TestB: entrez un nombre " <<  endl;
+				cin >> ValB; 
 
-			//case 'C':
-			//case 'c':
-			//	cout << "TestC: calculs surface " <<  endl;
+				// Appel de la fonction avec un seul paramètre
+				AfficheB(ValB);
+
+				// Appel de la fonction avec le 2ème paramètre 'H'
+				AfficheB(ValB, 'H');
+
+				// Appel de la fonction avec 'H' et 8
+				AfficheB(ValB, 'H', 8);
+
+			break;
+
+			case 'C':
+			case 'c':
+			cout << "TestC: calculs surface " <<  endl;
 
 				// a)	Avec 2 paramètres int 25 et 45
+				cout << "int hauteur = 25   int largeur = 45   surface = " << CalculSurfaceRectangleInt(25, 45) << endl;
 				
 				// b)	Avec 2 paramètres double 23.25 et 44.33'
-				
+				cout << "double hauteur = 23.25   double largeur = 44.33   surface = " << CalculSurfaceRectangleDouble(23.25, 44.33) << endl;
+
 				// c)	Avec 4 paramètres int 20,20, 60,40
-				
+				std::cout << "X1 = 20, Y1 = 20, X2 = 60, Y2 = 40   surface = " << CalculSurfaceRectangleAvecQuatreArguments(20, 20, 60, 40) << std::endl;
 
 			break;
 
